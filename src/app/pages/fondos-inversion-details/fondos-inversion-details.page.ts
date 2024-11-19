1c1e09542cd1b5d59a54c2f69a6f4e54d0a01524
Sergio <sergiogallego@xerintel.es>
2024-11-19 07:26:23 +0100

    gráficas

--- src/app/pages/fondos-inversion-details/fondos-inversion-details.page.ts
+++ src/app/pages/fondos-inversion-details/fondos-inversion-details.page.ts
@@ -31,66 +31,95 @@ export class FondosInversionDetailsPage implements OnInit {
       new Chart(ctx, {
         type: 'line',
         data: {
-          labels: ['2022', '2023', '2024'],
+          labels: ['', '', '', '', '2022', '', '', '', '', '2023', '', '', '', '', '2024', '', '',],
           datasets: [
             {
               label: 'A&B',
-              data: [5, 10, 15, -5, 0, 10, -10, 5, -5, 20, 10, 0],
+              data: [0, 10, 8, 17, 7, 16, 28, 15, 21, 17, 25, 20, 23, 7, 22, 10, 17],
               borderColor: '#FFD700',
               backgroundColor: 'rgba(255, 215, 0, 0.2)',
               fill: false,
-              tension: 0.4
+              tension: 0,
+              pointRadius: 0,
+              borderWidth: 2,
             },
             {
               label: 'Indx A&B 400',
-              data: [-10, -5, 10, 5, -5, 0, 10, 15, 5, 0, -10, 10],
+              data: [0, -5, 0, -10, 20, 15, -3, 15, 5, 10, 0, 4, 2, 20, 4, 18, 10],
               borderColor: '#1E90FF',
               backgroundColor: 'rgba(30, 144, 255, 0.2)',
               fill: false,
-              tension: 0.4
+              tension: 0,
+              pointRadius: 0,
+              borderWidth: 2,
             },
             {
               label: 'App Inc.',
-              data: [-5, 15, 0, 10, -10, 5, -5, 0, 15, -10, 5, -5],
+              data: [0, -15, -10, -18, 10, 5, -10, 23, 15, 20, 10, 15, 11, 26, 15, 25, 15],
               borderColor: '#87CEFA',
               backgroundColor: 'rgba(135, 206, 250, 0.2)',
               fill: false,
-              tension: 0.4
+              tension: 0,
+              pointRadius: 0,
+              borderWidth: 2,
             },
             {
               label: 'S.E Ab',
-              data: [10, -10, 5, 15, 0, -5, 10, 0, -15, 5, -5, 15],
+              data: [0, -15, -10, -18, -7, -16, -30, -15, -20, -17, -27, -2, -5, 10, -3, 10, 0],
               borderColor: '#4682B4',
               backgroundColor: 'rgba(70, 130, 180, 0.2)',
               fill: false,
-              tension: 0.4
+              tension: 0,
+              pointRadius: 0,
+              borderWidth: 2,
             }
           ]
         },
         options: {
           responsive: true,
           plugins: {
+            datalabels: {
+              display: false, // Hace que no se muestren los número sobre la línea de la gráfica
+            },
             legend: {
               display: false,
               position: 'bottom'
-            }
+            },
+            tooltip: {
+              enabled: false // Desactiva los tooltips
+          }
           },
           scales: {
             y: {
               beginAtZero: false,
-              min: -20,
-              max: 20,
+              min: -40,
+              max: 40,
               ticks: {
-                stepSize: 10,
+                stepSize: 20,
                 callback: function(value) {
-                  return value + '%';
+                  // return value + '%';
+                  // Evitar mostrar el símbolo '%' cuando el valor es 40
+                if (value === 40 || value === -40) {
+                  return "";  // Devuelve solo el valor sin el símbolo '%'
                 }
-              }
+                return String(value) + '%';  // Agrega el símbolo '%' a otros valores
+                },
+                 display: true
+              },
+              grid: {
+                color: '#444', // Color de las líneas de la cuadrícula
+                tickLength: 20,
+                lineWidth: 1, 
+                
+              },
             },
+
+            
+
             x: {
               display: true
             }
-          }
+          },
         }
       });
     }
